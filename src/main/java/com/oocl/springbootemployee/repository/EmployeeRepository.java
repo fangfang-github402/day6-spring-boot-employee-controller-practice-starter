b452bcdd6d71f196d50bf56875665af14099be88
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-11-25 15:48:00 +0800

    test:should return employees when get all given employees & should return employee when get by id given employee id

--- src/main/java/com/oocl/springbootemployee/repository/EmployeeRepository.java
+++ src/main/java/com/oocl/springbootemployee/repository/EmployeeRepository.java
@@ -15,10 +15,13 @@ public class EmployeeRepository {
         employees.add(new Employee(1,"Tom1",18, Gender.MALE,5000));
         employees.add(new Employee(2,"Tom2",18,Gender.MALE,7000));
         employees.add(new Employee(3,"Tom3",18,Gender.MALE,6000));
-        employees.add(new Employee(4,"Tom4",18,Gender.FEMALE,5000));
     }
 
     public List<Employee> getAll() {
         return employees;
+    }
+
+    public Employee getById(Integer id) {
+        return employees.stream().filter(employee -> employee.getId().equals(id)).findFirst().orElseThrow();
     }
 }
---
+++ src/test/java/com/oocl/springbootemployee/controller/EmployeeControllerTest.java
@@ -0,0 +1,77 @@
+package com.oocl.springbootemployee.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oocl.springbootemployee.model.Employee;
+import com.oocl.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+
+@AutoConfigureJsonTesters
+@AutoConfigureMockMvc
+@SpringBootTest
+class EmployeeControllerTest {
+    @Autowired
+    private MockMvc client;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private JacksonTester<Employee> json;
+    @Autowired
+    private JacksonTester<List<Employee>> listJson;
+
+    @Test
+    void should_return_employee_when_get_all_given_employees() throws Exception {
+        //Given
+        final List<Employee> givenEmployees = employeeRepository.getAll();
+        //When
+        //Then
+//        client.perform(MockMvcRequestBuilders.get("/employees"))
+//                .andExpect(MockMvcResultMatchers.status().isOk())
+//                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(3)))
+//                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value(givenEmployees.get(0).getName()))
+//                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value(givenEmployees.get(1).getName()))
+//                .andExpect(MockMvcResultMatchers.jsonPath("$[2].name").value(givenEmployees.get(2).getName()));
+
+        String employeesJSONString = client.perform(MockMvcRequestBuilders.get("/employees"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(3)))
+                .andReturn().getResponse().getContentAsString();
+
+        List<Employee> employeeList = listJson.parseObject(employeesJSONString);
+        assertThat(employeeList)
+                .usingRecursiveComparison()
+                .isEqualTo(givenEmployees);
+
+    }
+
+    @Test
+    void should_return_employee_when_get_by_id_given_employee_id() throws Exception {
+        //Given
+        final List<Employee> givenEmployees = employeeRepository.getAll();
+        final Integer employeeId = givenEmployees.get(0).getId();
+        //When
+        //Then
+        String employeeJson = client.perform(MockMvcRequestBuilders.get("/employees/" + employeeId))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+
+        Employee returnedEmployee = json.parseObject(employeeJson);
+        assertThat(returnedEmployee.getId()).isEqualTo(employeeId);
+    }
+}
